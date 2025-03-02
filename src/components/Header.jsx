9da1cd2c12f8eb5edba34de6c889f6490e1bc4bc
Sadhana-Jagan <sadhanajagan2004@gmail.com>
2025-03-02 17:00:41 +0530

    please be the final commit for img(3)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <div className="header">
-            <img src={`${process.env.PUBLIC_URL}/images/react-logo.png`} alt="React Logo" />
+            <img src="/fun-facts-about-react/images/react-logo.png" alt="React Logo" />
             <h2 className="facts-heading">ReactFacts</h2>
         </div>
     );
