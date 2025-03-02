36c34488ee61c75b96e5614afcbd04af965bbad3
Sadhana-Jagan <sadhanajagan2004@gmail.com>
2025-03-02 16:19:54 +0530

    mostly final commit for image routing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@
 export default function Header() {
     return (
         <div className="header">
-            <img src="public/images/react-logo.png" alt="react-logo" />
+            <img src="/images/react-logo.png" alt="react-logo" />
             <h2 className="facts-heading">ReactFacts</h2>
         </div>
     )
